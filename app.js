559d39792424124c106a4dc3ffa16e2baa6558d8
divyamtayal <divyamtayal18@gmail.com>
2023-01-11 03:27:27 +0530

    Add Security check for auth

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const monogoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const userRouter = require('./routes/userRoutes');
@@ -7,12 +12,34 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// Middlewares
+// Global Middlewares
+// Set Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit request from same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour'
+});
+app.use('/api', limiter);
+
+// Body Parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NOSQL Query injection
+app.use(monogoSanitize());
+
+// Data sanitization against xSS attack
+app.use(xss());
+
+// Prevent Parameter Pollution
+app.use(hpp());
 
 // Routes
 app.use('/api/v1/users', userRouter);
