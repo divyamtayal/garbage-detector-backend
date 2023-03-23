e8c94b1d16c03eeb5499272b204cad7b649f7ea0
divyamtayal <divyamtayal18@gmail.com>
2023-03-24 03:37:26 +0530

    Fix

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 mongoose.set('strictQuery', true);
 mongoose
   .connect(DB, {
