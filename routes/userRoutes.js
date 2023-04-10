61abe00b7ab992cc19d7d746f226d4805499dc2a
divyamtayal <divyamtayal18@gmail.com>
2023-04-11 03:45:01 +0530

    Added Get User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,13 +18,13 @@ router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
-  .route('/')
+  .route('/getAllUsers')
   .get(
     authController.protect,
     authController.restrictTo('admin'),
     userController.getAllUsers
   );
 
-router.route('/:id').get(authController.protect, userController.getUser);
+router.route('/').get(authController.protect, userController.getUser);
 
 module.exports = router;
