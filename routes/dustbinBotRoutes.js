74577a1dd72e589e5dd5613d3b494365b95d1616
divyamtayal <divyamtayal18@gmail.com>
2023-05-10 19:34:11 +0530

    Change to GET

--- routes/dustbinBotRoutes.js
+++ routes/dustbinBotRoutes.js
@@ -4,7 +4,7 @@ const dustbinBotController = require('./../controllers/dustbinBotController');
 const router = express.Router();
 
 router.post('/register', dustbinBotController.registerDustbinBot);
-router.post('/updateStatus', dustbinBotController.updateDustbinBotStatus);
+router.get('/updateStatus', dustbinBotController.updateDustbinBotStatus);
 router.get('/getAll', dustbinBotController.getAllDustbinBots);
 router.get('/getDustbinBot', dustbinBotController.getDustbinBot);
 
